e87c06cb87c08562e78ba22e295a1f8add2681cd
Pushkar-sharma28 <pushkarsharma8800@gmail.com>
2022-07-14 20:35:26 +0530

    Hashing

---
+++ Hashing/9.Group_anagrams.cpp
@@ -0,0 +1,19 @@
+LEETCODE 49
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map<string,vector<string>> mpp;
+        vector<vector<string>> ans;
+        for(auto x: strs){
+         string temp=x;
+         sort(x.begin(),x.end());
+         mpp[x].push_back(temp);
+        }
+        for(auto it: mpp){
+         vector<string> temp=it.second;
+         ans.push_back(temp);
+        }
+        return ans;
+    }
+};
