65cbd996ccde37fb32c93d5371ed28c4163f2ce4
Pushkar-sharma28 <pushkarsharma8800@gmail.com>
2022-07-17 17:50:15 +0530

    dp

---
+++ Dynamic programming/1-D_dp/fibonacci.cpp
@@ -0,0 +1,72 @@
+//Normal recursive pseudo code of fibonacci series
+fibo(int n)
+{
+    if(n<=1) return n;
+    return fibo(n-1)+fibo(n-2);
+}
+
+//Now applying memoization on this code 
+//it means to save state
+
+//Memoization code of fibo series
+//making a dp array and saving repeating function call
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n, vector<int>& dp){
+    if(n<=1) return n;
+    
+    if(dp[n]!= -1) return dp[n];
+    return dp[n]= f(n-1,dp) + f(n-2,dp);
+}
+
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  cout<<f(n,dp);
+  return 0;
+}
+
+//Tabulation
+//-it is the bottom to up approach
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 0;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
+
+//doing space optimization
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+  int n=5;
+  
+  int prev2 = 0;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
