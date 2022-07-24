ab3388721aa2706529fab498ae48481205b185a6
Pushkar-sharma28 <pushkarsharma8800@gmail.com>
2022-07-24 19:08:18 +0530

    Hashing

---
+++ Hashing/1.Unorderedset.cpp
@@ -0,0 +1,84 @@
+
+//header file of set
+#include<unordered_set>
+
+/* a set is implemented using hashtable 
+all operations take O(1) at average that is constant time
+set operations logn generally
+
+//this doesn't happens generally
+in the worst case it can go O(n)
+but it depends on the internal hash funciton*/
+
+//set is implemented usign red black tree
+//whereas unordered set is implemented using hashing 
+
+//set doesn't have duplicates
+
+insertion 
+unordered_set<int> s;
+s.insert(10);
+s.insert(15);
+
+s.begin()-> a pointer to start __value
+s.end()-> a pointer for the end value
+
+//printing the elements of a set
+for(auto it =s.begin() ;it!=s.end();it++)
+cout<<s.(*it)<<endl;
+
+size of set -> s.size()
+
+code for set
+
+#include<bits/stdc++.h>
+#include<unordered_set>
+
+using namespace std;
+int main(){
+
+    unordered_set<int> s;
+    s.insert(10);
+    s.insert(5);
+    s.insert(15);
+    s.insert(20);
+    
+    for(auto it = s.begin() ; it!=s.end() ;it++)
+        cout<<(*it)<<endl;
+    
+    cout<<"number of element : "<<s.size()<<endl;
+
+    s.clear();
+    cout<<"number of element : "<<s.size()<<endl;
+
+
+    int key = 20;
+
+    
+    if(s.find(key) == s.end()){
+        cout<<"key not found"<<endl;
+    }else{
+        
+        auto temp = s.find(key);
+        s.erase(temp);
+
+    }
+
+    int deleted_key = 25;
+    s.erase(deleted_key);
+
+    for(auto it = s.begin() ; it!=s.end() ;it++)
+        cout<<(*it)<<endl;
+
+    cout<<"number of element : "<<s.size()<<endl;
+
+
+    if(s.find(key)!=s.end())
+        cout<<"key found"<<endl;
+
+
+    if(s.count(5))
+        cout<<"key found"<<endl;
+    else
+        cout<<"not found"<<endl;
+}
